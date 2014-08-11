560a0a80fb5ece26b5ac9b18328e00a5df458444
leeyeh <lee55962698@gmail.com>
2014-08-11 16:16:45 +0800

    add support for conf.file as array.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 module.exports = function(content, file, conf) {
-    if (conf && fis.util.is(conf.file, 'String')) {
-        return '@import "' + conf.file + '";\n' + content;
-    }
-    else {
-        fis.log.error('Param file{String} required. Please check settings.parser["less-import"].');
-    }
+	if (!fis.util.is(conf.file, 'Array')) {
+		conf.file = [conf.file];
+	}
+
+	var importText = '';
+	for (var i = 0; i < conf.file.length; i++) {
+		if (conf && fis.util.is(conf.file[i], 'String')) {
+			importText += ('@import "' + conf.file[i] + '";\n');
+		} else {
+			fis.log.error('Param file{String|Array} required. Please check settings.parser["less-import"].');
+		}
+	};
+	
+	return importText + content;
 };
